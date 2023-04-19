09b21db7686fd496d14ec60880aad833200511cf
heena kapuriya <heenap583@gmail.com>
2023-04-19 16:36:25 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi aut provident excepturi expedita aliquid fuga suscipit non aperiam dolorem nostrum libero tenetur ea perspiciatis quo architecto sit autem, odit sed a eius est soluta reprehenderit ipsam accusantium. Reprehenderit vero ut harum, totam itaque inventore autem veniam dicta architecto neque id quaerat ipsum culpa rem temporibus eveniet illo iusto, alias, voluptates error! Maxime itaque quia perferendis molestiae, eaque at reiciendis eos nulla, minima vitae cumque molestias fuga perspiciatis ex temporibus. Eveniet odio maxime placeat, numquam accusamus modi ratione aperiam temporibus aliquid beatae! Quam nemo porro iure quis, deleniti dolore repudiandae libero.</p>
+  )
+}
+
+export default App
