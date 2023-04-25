7bf16b31326e976d7ccceedaefd02589a68e7338
heena kapuriya <heenap583@gmail.com>
2023-04-25 21:38:07 +0530

    navbar create

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+// import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-   <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi aut provident excepturi expedita aliquid fuga suscipit non aperiam dolorem nostrum libero tenetur ea perspiciatis quo architecto sit autem, odit sed a eius est soluta reprehenderit ipsam accusantium. Reprehenderit vero ut harum, totam itaque inventore autem veniam dicta architecto neque id quaerat ipsum culpa rem temporibus eveniet illo iusto, alias, voluptates error! Maxime itaque quia perferendis molestiae, eaque at reiciendis eos nulla, minima vitae cumque molestias fuga perspiciatis ex temporibus. Eveniet odio maxime placeat, numquam accusamus modi ratione aperiam temporibus aliquid beatae! Quam nemo porro iure quis, deleniti dolore repudiandae libero.</p>
+    <div>
+    <h2 className="bg-danger">hello</h2>
+    <h2 className="bg-info">hello</h2>
+    <p>hello</p>
+    </div>
+    // <div className="container">
+    //   <div className="fom">
+    //     <form action="">
+    //       <h1>login Form</h1>
+
+    //       <div className="fom-1">
+    //         <label htmlFor="">Email or Phone</label><br />
+    //         <input type="text" /><br />
+    //       </div>
+
+    //       <div className="fom-1">
+    //         <label htmlFor="">Password</label><br />
+    //         <input type="password" /><br />
+    //         <label htmlFor="" className='lab'>Forget password?</label>
+    //       </div>
+
+    //       <button>LOGIN</button>
+
+    //       <footer>Not a member? Signup Now</footer>
+    //     </form>
+    //   </div>
+    // </div>
+  
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+// import App from './App'
+import Navbar from './Componants/Navbar'
+// import './index.css'
+import "@popperjs/core/dist/umd/popper"
+import "bootstrap/dist/js/bootstrap"
+import "bootstrap/dist/css/bootstrap.css"
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+    // <App />
+    <Navbar></Navbar>
+  
 )
